459baeafe88288ef6c8468892d3548dea778ab64
Stelios Kapsis <stelios.kapsis.98@gmail.com>
2025-05-27 18:03:04 +0300

    second commit,removed outliers, added Cross Validation and Saved the model for later use

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+import subprocess
 
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error
@@ -10,7 +12,6 @@ color_pal = sns.color_palette()
 plt.style.use('fivethirtyeight')
 
 df = pd.read_csv(r'C:\Users\steli\Desktop\AI - Time Series\data\PJME_hourly.csv')
-
 df = df.set_index('Datetime')
 df.index = pd.to_datetime(df.index)
 
@@ -18,10 +19,20 @@ df.plot(style='.',
         figsize=(15, 5),
         color=color_pal[0],
         title='PJME Energy Use in MW')
-#plt.show()
+plt.show()
 
+# Outlier Analysis and removal
+df['PJME_MW'].plot(kind='hist', bins=500)
 
-# Train / Test Split
+df.query('PJME_MW < 19_000')['PJME_MW'] \
+    .plot(style='.',
+          figsize=(15, 5),
+          color=color_pal[5],
+          title='Outliers')
+
+df = df.query('PJME_MW > 19_000').copy()
+
+# Reviewing: Train / Test Split
 train = df.loc[df.index < '01-01-2015']
 test = df.loc[df.index >= '01-01-2015']
 
@@ -30,23 +41,35 @@ train.plot(ax=ax, label='Training Set', title='Data Train/Test Split')
 test.plot(ax=ax, label='Test Set')
 ax.axvline('01-01-2015', color='black', ls='--')
 ax.legend(['Training Set', 'Test Set'])
-#plt.show()
+plt.show()
 
-weeks_df = df.loc[(df.index > '01-01-2010') & (df.index < '01-08-2010')]
-# Ensure index is datetime and sorted
-weeks_df = weeks_df.sort_index()
+# Time Series Cross Validation
+from sklearn.model_selection import TimeSeriesSplit
 
-# Check for duplicate timestamps
-duplicates = weeks_df.index.duplicated().sum()
-#print(f"Number of duplicate timestamps: {duplicates}")
+tss = TimeSeriesSplit(n_splits=5, test_size=24*365*1, gap=24)
+df = df.sort_index()
 
-#print(weeks_df.index[:10])  # First 10 timestamps
-#print(weeks_df.index[-10:])  # Last 10 timestamps
+fig, axs = plt.subplots(5, 1, figsize=(15, 15), sharex=True)
 
-weeks_df.plot(figsize=(15, 5), title='Week Of Data') 
-#plt.show()
+fold = 0
+for train_idx, val_idx in tss.split(df):
+    train = df.iloc[train_idx]
+    test = df.iloc[val_idx]
+    train['PJME_MW'].plot(ax=axs[fold],
+                          label='Training Set',
+                          title=f'Data Train/Test Split Fold {fold}')
+    test['PJME_MW'].plot(ax=axs[fold],
+                         label='Test Set')
+    axs[fold].axvline(test.index.min(), color='black', ls='--')
+    fold += 1
+plt.show()
 
-# Feature Extraction
+# Forecasting Horizon Explained
+'''
+ The forecast horizon is the length of time into the future for which forecasts are to 
+ be prepared. These generally vary from short-term forecasting horizons 
+ (less than three months) to long-term horizons (more than two years).
+'''
 def create_features(df):
     """
     Create time series features based on time series index.
@@ -64,71 +87,118 @@ def create_features(df):
 
 df = create_features(df)
 
-# Visualize our Feature / Target Relationship
-# Visualisation by Hour
-fig, ax = plt.subplots(figsize=(10, 8))
-sns.boxplot(data=df, x='hour', y='PJME_MW')
-ax.set_title('MW by Hour')
-#plt.show()
+# Lag Features
+def add_lags(df):
+    target_map = df['PJME_MW'].to_dict()
+    df['lag1'] = (df.index - pd.Timedelta('364 days')).map(target_map)
+    df['lag2'] = (df.index - pd.Timedelta('728 days')).map(target_map)
+    df['lag3'] = (df.index - pd.Timedelta('1092 days')).map(target_map)
+    return df
 
-# Visualisation by Month
-fig, ax = plt.subplots(figsize=(10, 8))
-sns.boxplot(data=df, x='month', y='PJME_MW', palette='Blues')
-ax.set_title('MW by Month')
-#plt.show()
+df = add_lags(df)
 
-# Model Creation
-train = create_features(train)
-test = create_features(test)
+#Train Using Cross Validation
+tss = TimeSeriesSplit(n_splits=5, test_size=24*365*1, gap=24)
+df = df.sort_index()
 
-FEATURES = ['dayofyear', 'hour', 'dayofweek', 'quarter', 'month', 'year']
+
+fold = 0
+preds = []
+scores = []
+for train_idx, val_idx in tss.split(df):
+    train = df.iloc[train_idx]
+    test = df.iloc[val_idx]
+
+    train = create_features(train)
+    test = create_features(test)
+
+    FEATURES = ['dayofyear', 'hour', 'dayofweek', 'quarter', 'month','year',
+                'lag1','lag2','lag3']
+    TARGET = 'PJME_MW'
+
+    X_train = train[FEATURES]
+    y_train = train[TARGET]
+
+    X_test = test[FEATURES]
+    y_test = test[TARGET]
+
+    reg = xgb.XGBRegressor(base_score=0.5, booster='gbtree',    
+                           n_estimators=1000,
+                           early_stopping_rounds=50,
+                           objective='reg:linear',
+                           max_depth=3,
+                           learning_rate=0.01)
+    reg.fit(X_train, y_train,
+            eval_set=[(X_train, y_train), (X_test, y_test)],
+            verbose=100)
+
+    y_pred = reg.predict(X_test)
+    preds.append(y_pred)
+    score = np.sqrt(mean_squared_error(y_test, y_pred))
+    scores.append(score)
+
+print(f'Score across folds {np.mean(scores):0.4f}')
+print(f'Fold scores:{scores}')
+
+#Predicting the Future
+# Retrain on all data
+df = create_features(df)
+
+FEATURES = ['dayofyear', 'hour', 'dayofweek', 'quarter', 'month', 'year',
+            'lag1','lag2','lag3']
 TARGET = 'PJME_MW'
 
-X_train = train[FEATURES]
-y_train = train[TARGET]
+X_all = df[FEATURES]
+y_all = df[TARGET]
 
-X_test = test[FEATURES]
-y_test = test[TARGET]
-
-reg = xgb.XGBRegressor(base_score=0.5, booster='gbtree',    
-                       n_estimators=1000,
-                       early_stopping_rounds=50,
+reg = xgb.XGBRegressor(base_score=0.5,
+                       booster='gbtree',    
+                       n_estimators=500,
                        objective='reg:linear',
                        max_depth=3,
                        learning_rate=0.01)
-reg.fit(X_train, y_train,
-        eval_set=[(X_train, y_train), (X_test, y_test)],
+reg.fit(X_all, y_all,
+        eval_set=[(X_all, y_all)],
         verbose=100)
 
-# Feature Importance
+df.index.max()
 
-fi = pd.DataFrame(data=reg.feature_importances_,
-             index=reg.feature_names_in_,
-             columns=['importance'])
-fi.sort_values('importance').plot(kind='barh', title='Feature Importance')
-#plt.show()
+# Create future dataframe
+future = pd.date_range('2018-08-03','2019-08-01', freq='1h')
+future_df = pd.DataFrame(index=future)
+future_df['isFuture'] = True
+df['isFuture'] = False
+df_and_future = pd.concat([df, future_df])
+df_and_future = create_features(df_and_future)
+df_and_future = add_lags(df_and_future)
 
-# Forecast on Test
-test['prediction'] = reg.predict(X_test)
-df = df.merge(test[['prediction']], how='left', left_index=True, right_index=True)
-ax = df[['PJME_MW']].plot(figsize=(15, 5))
-df['prediction'].plot(ax=ax, style='.')
-plt.legend(['Truth Data', 'Predictions'])
-ax.set_title('Raw Dat and Prediction')
+future_w_features = df_and_future.query('isFuture').copy()
+
+#Predict the future
+future_w_features['pred'] = reg.predict(future_w_features[FEATURES])
+
+future_w_features['pred'].plot(figsize=(10, 5),
+                               color=color_pal[4],
+                               ms=1,
+                               lw=1,
+                               title='Future Predictions')
 plt.show()
 
-ax1 = df.loc[(df.index > '04-01-2018') & (df.index < '04-08-2018')]['PJME_MW']
-ax1 = ax1.sort_index() \
-    .plot(figsize=(15, 5), title='Week Of Data')
-df.loc[(df.index > '04-01-2018') & (df.index < '04-08-2018')]['prediction'] \
-    .plot(style='.')
-plt.legend(['Truth Data','Prediction'])
-plt.show()
+# Save model
+reg.save_model('model.json')
 
-score = np.sqrt(mean_squared_error(test['PJME_MW'], test['prediction']))
-print(f'RMSE Score on Test set: {score:0.2f}')
+# List files in current directory (Windows)
+# subprocess.run(['dir'], shell=True)  # For Windows
+# Or for Unix/Linux:
+subprocess.run('dir', shell=True)
 
-test['error'] = np.abs(test[TARGET] - test['prediction'])
-test['date'] = test.index.date
-print(test.groupby(['date'])['error'].mean().sort_values(ascending=False))
-print(test.groupby(['date'])['error'].mean().sort_values(ascending=True))
+# Load model
+reg_new = xgb.XGBRegressor()
+reg_new.load_model('model.json')
+
+# Predict
+future_w_features['pred'] = reg_new.predict(future_w_features[FEATURES])
+future_w_features['pred'].plot(figsize=(10, 5),
+                               color=color_pal[4],
+                               ms=1, lw=1,
+                               title='Future Predictions')
